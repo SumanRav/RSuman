d8a9d273b5f2424fea1fca6285f79d09591bccbd
sumrav <sumrav@DIN73003007.corp.capgemini.com>
2018-11-23 14:24:11 +0530

    Greetings!!!

---
+++ GitDemoProject/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,14 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		greetings();
+	}
+	
+	public static void greetings() {
+		System.out.println("Welcome to Github!!");
+	}
+
+}
